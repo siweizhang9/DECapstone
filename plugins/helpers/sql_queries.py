cc8934085b7b214d0987351d7b65becfce4b50a9
siweizhang9 <31421213+siweizhang9@users.noreply.github.com>
2023-04-27 01:38:41 -0400

    Update sql_queries.py
    Removed "DROP" statements in the create_all_table query, also add " CREATE IF NOT EXISTS" to the current creating table statements

--- plugins/helpers/sql_queries.py
+++ plugins/helpers/sql_queries.py
@@ -43,15 +43,7 @@ class SqlQueries:
 
     create_all_tables =(
         """
-    DROP TABLE IF EXISTS public.artists;
-    DROP TABLE IF EXISTS public.songplays;
-    DROP TABLE IF EXISTS public.songs;
-    DROP TABLE IF EXISTS public.staging_events;
-    DROP TABLE IF EXISTS public.staging_songs;
-    DROP TABLE IF EXISTS public.time;
-    DROP TABLE IF EXISTS public.users;
-
-    CREATE TABLE public.artists (
+    CREATE TABLE IF NOT EXISTS public.artists (
 	artistid varchar(256) NOT NULL,
 	name varchar(256),
 	location varchar(256),
@@ -59,7 +51,7 @@ class SqlQueries:
 	longitude numeric(18,0)
     );
 
-    CREATE TABLE public.songplays (
+    CREATE TABLE IF NOT EXISTS public.songplays (
         playid varchar(32) NOT NULL,
         start_time timestamp NOT NULL,
         userid int4 NOT NULL,
@@ -72,7 +64,7 @@ class SqlQueries:
         CONSTRAINT songplays_pkey PRIMARY KEY (playid)
     );
 
-    CREATE TABLE public.songs (
+    CREATE TABLE IF NOT EXISTS public.songs (
         songid varchar(256) NOT NULL,
         title varchar(256),
         artistid varchar(256),
@@ -81,7 +73,7 @@ class SqlQueries:
         CONSTRAINT songs_pkey PRIMARY KEY (songid)
     );
 
-    CREATE TABLE public.staging_events (
+    CREATE TABLE IF NOT EXISTS public.staging_events (
         artist varchar(256),
         auth varchar(256),
         firstname varchar(256),
@@ -102,7 +94,7 @@ class SqlQueries:
         userid int4
     );
 
-    CREATE TABLE public.staging_songs (
+    CREATE TABLE IF NOT EXISTS public.staging_songs (
         num_songs int4,
         artist_id varchar(256),
         artist_name varchar(256),
@@ -115,7 +107,7 @@ class SqlQueries:
         "year" int4
     );
 
-    CREATE TABLE public."time" (
+    CREATE TABLE IF NOT EXISTS public."time" (
         start_time timestamp NOT NULL,
         "hour" int4,
         "day" int4,
@@ -126,7 +118,7 @@ class SqlQueries:
         CONSTRAINT time_pkey PRIMARY KEY (start_time)
     );
 
-    CREATE TABLE public.users (
+    CREATE TABLE IF NOT EXISTS public.users (
         userid int4 NOT NULL,
         first_name varchar(256),
         last_name varchar(256),
